19d6148d11297ef17f4421a5da0da120f9a05a22
Arafat-erasung <arafaterasung@gmail.com>
2025-12-29 12:39:29 +0000

    created the call back function

--- network_sniffer.py
+++ network_sniffer.py
@@ -5,3 +5,23 @@ Task 1: Network Packet Capture and Analysis
 """
 
 from scapy.all import sniff, IP, TCP, UDP, ICMP, Raw
+import datetime
+
+def packet_callback(packet):
+    """
+    callback function to process each captured packet
+    """
+
+    #get current time
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("\n" + "="*70)
+    print(f"[{timestamp}] PACKET CAPTURED")
+    print("="*70)
+
+    if packet.haslayer(IP):
+        ip_layer = packet[IP]
+
+        print(f"Source IP:      {ip_layer.src}")
+        print(f"Destination IP: {ip_layer.dst}")
+        print(f"Protocol:       {ip_layer.proto}")
+        print(f"Packet Length:  {len(packet)} bytes")
