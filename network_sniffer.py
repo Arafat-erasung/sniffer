e97f401fb2cd7688c7f197991e80edca92e88802
Arafat-erasung <arafaterasung@gmail.com>
2025-12-29 12:26:22 +0000

    created a docstring

---
+++ network_sniffer.py
@@ -0,0 +1,7 @@
+"""
+Basic Network Sniffer - CodeAlpha Cybersecurity Internship
+Author: Arafat Erasung
+Task 1: Network Packet Capture and Analysis
+"""
+
+from scapy.all import sniff, IP, TCP, UDP, ICMP, Raw
